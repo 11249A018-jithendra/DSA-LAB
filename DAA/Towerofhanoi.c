4b3e0eb800167f6ee15c7254a6ed15e02cb5c5ec
Jithendra <11249a018@kanchiuniv.ac.in>
2026-02-07 12:19:57 +0530

    Refactor Tower of Hanoi implementation
    Updated comments for clarity and improved code structure.

--- DAA/Towerofhanoi.c
+++ DAA/Towerofhanoi.c
@@ -1,26 +1,44 @@
 //Aim:Implementation of tower of hanoi using recursion.
 
 //program:
-#include<stdio.h>
+#include <stdio.h>
 
-void towerofhanoi(int n,char s,char d,char a)
+// Function to solve Tower of Hanoi problem
+// n  -> number of disks
+// s  -> source rod
+// d  -> destination rod
+// a  -> auxiliary rod
+void towerofhanoi(int n, char s, char d, char a)
 {
-    if(n==1)
+    // Base case: if there is only one disk
+    if (n == 1)
     {
-        printf("move the disk %d from %c to %c\n",n,s,d);
-     return;
+        // Move the single disk directly from source to destination
+        printf("move the disk %d from %c to %c\n", n, s, d);
+        return;
     }
-    towerofhanoi(n-1,s,a,d);
-    printf("move disk %d from %c to %c\n",n,s,d);
-     towerofhanoi(n-1,a,d,s);
+
+    // Step 1: Move (n-1) disks from source to auxiliary rod
+    towerofhanoi(n - 1, s, a, d);
+
+    // Step 2: Move the nth (largest) disk from source to destination
+    printf("move disk %d from %c to %c\n", n, s, d);
+
+    // Step 3: Move the (n-1) disks from auxiliary rod to destination
+    towerofhanoi(n - 1, a, d, s);
 }
+
 int main()
 {
     int disk_count;
+
+    // Ask the user for the number of disks
     printf("enter how many disk are there:\n");
-    scanf("%d",&disk_count);
-    printf("reqired moves:\n");
-    towerofhanoi(disk_count,'s','d','a');
+    scanf("%d", &disk_count);
+    printf("required moves:\n");
+    towerofhanoi(disk_count, 's', 'd', 'a');
+
+    return 0;
 }
 
 
